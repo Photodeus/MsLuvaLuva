e87cba2f2cda58116c8275a924c8ad6cbd8e8347
Photodeus <photodeus+github@gmail.com>
2009-10-10 18:27:49 +0200

    javadoc

--- src/com/popodeus/chat/ScriptAPI.java
+++ src/com/popodeus/chat/ScriptAPI.java
@@ -21,24 +21,68 @@ public interface ScriptAPI {
 	 * @param nick
 	 * @param ident may be null
 	 * @param host may be null
-	 * @see {@link #isIgnored(String, String, String)} 
+	 * @see #isIgnored(String, String, String)
+	 * @see #removeIgnore(String, String, String)
 	 */
 	boolean addIgnore(String nick, String ident, String host);
+
+	/**
+	 * Adds someone to command ignore for specified time in minutes
+	 * @param nick
+	 * @param ident
+	 * @param host
+	 * @param minutes
+	 * @see #removeIgnore(String, String, String)
+v	 */
 	boolean addIgnore(String nick, String ident, String host, int minutes);
+
+	/**
+	 * Removes a previously set ignore
+	 * @see #addIgnore(String, String, String)
+	 */
 	boolean removeIgnore(String nick, String ident, String host);
+
+	/**
+	 * Checks if someone is ignored
+	 * @see #addIgnore(String, String, String)
+	 */
 	boolean isIgnored(String nick, String ident, String host);
 	String[] listIgnores();
 
 	User[] getUsers(String channel);
 	/**
+	 * Reads a Page (object) from the web. JavaScript is disabled when fetching
+	 * pages.
 	 * @see com.gargoylesoftware.htmlunit.html.HtmlPage
 	 * @see com.gargoylesoftware.htmlunit.xml.XmlPage 
 	 */
 	Page getPage(String url);
+
+	/**
+	 * Get page contents as text, no matter if it's html, xml, text, json etc.
+	 * @param url
+	 * @return
+	 */
 	String getPageAsText(String url);
-	String getAsText(Page url, String xpath);
-	DomElement getByXpath(Page url, String xpath);
+
+	/**
+	 * Get specified node from html or xml page as text
+	 * @param page Page object that was returned by {@link #getPage(String)}
+	 * @param xpath XPath expression pointing to the node.
+	 */
+	String getAsText(Page page, String xpath);
+	/**
+	 * Get specified node from html or xml page
+	 * @param page Page object that was returned by {@link #getPage(String)}
+	 * @param xpath XPath expression pointing to the node.
+	 */
+	DomElement getByXpath(Page page, String xpath);
+
+	/**
+	 * URLEncodes the string as UTF-8 escaped string, so it can be added to requests
+	 */
 	String encode(String param);
+
 	Element getElementById(Page page, String id);
 	List getElementsByAttribute(final Element parent, String tagname, String attribute, String value);
 
@@ -46,18 +90,20 @@ public interface ScriptAPI {
 	void setTimeout(String script, int timeout);
 
 	/**
-	 * Sets a value in global scope accessible to all scripts
+	 * Sets a value in global scope accessible to all scripts.
+	 * Meant to store variables so scripts can inter-communicate with each other.
 	 * @param key
 	 * @param value
 	 */
 	void setValue(String key, Object value);
 	/**
-	 * Gets a value from global scope which is accessible to all scripts
+	 * Gets a value from global scope which is accessible to all scripts.
+	 * Meant to store variables so scripts can inter-communicate with each other.
 	 * @param key
 	 */
 	Object getValue(String key);
 	/**
-	 * Removes a globally scoped value 
+	 * Removes a globally scoped value.
 	 * @param key
 	 */
 	Object removeValue(String key);
