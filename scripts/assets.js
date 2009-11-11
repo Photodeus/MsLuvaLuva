9f263c1f472b6a6cd479fb72350e4524db614535
Photodeus <photodeus+github@gmail.com>
2009-11-11 10:42:18 +0100

    using the properly named API

--- scripts/assets.js
+++ scripts/assets.js
@@ -2,5 +2,5 @@
 // @example !assets
 // @example !assets Jonas Zapatero
 // @example !assets 3 Mark Ward
-var line = ""+API.fetchUrl("http://popodeus.com/namesearch/find.jsp?a=" + API.encode(param));
+var line = ""+API.getPageAsText("http://popodeus.com/namesearch/find.jsp?a=" + API.encode(param));
 if (line) API.say(channel, line);
