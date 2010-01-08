022084d4c6102b01eb35abf779fccc753025983a
Photodeus <photodeus+github@gmail.com>
2010-01-08 23:09:50 +0100

    Refactored all PROP_* constants into a new Config interface from MsLuvaLuva main class

---
+++ src/com/popodeus/chat/Config.java
@@ -0,0 +1,26 @@
+package com.popodeus.chat;
+
+/**
+ * photodeus
+ * Jan 8, 2010
+ * 9:29:08 PM
+ */
+public interface Config {
+
+	public static final String NICK = "nick";
+	public static final String ALTNICK = "altnick";
+	public static final String GREETINGS_FILE = "greetings.file";
+	public static final String REJOINMSG = "rejoinmsg";
+	public static final String REJOINMSG_ENABLED = "rejoinmsg.enabled";
+	public static final String QUITMSG = "quitmsg";
+	public static final String VARIABLE_CACHE_DIR = "script.variable.dir";
+	public static final String SERVER = "server";
+	public static final String PASSWORD = "password";
+	public static final String CHANNELS = "channels";
+	public static final String GREET_CHANNELS = "channels.greet";
+	public static final String SCRIPT_DIR = "script.dir";
+	public static final String VERSION_STRING = "version.string";
+	public static final String TELNET_PORT = "telnet.port";
+	public static final String TELNET_PASSWORD = "telnet.password";
+	public static final String LOG_DIR = "log.dir";
+}
