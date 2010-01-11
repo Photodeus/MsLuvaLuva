5214d194331d3dd3ed7b37b2a529fc8a60ae2b26
Photodeus <photodeus+github@gmail.com>
2010-01-11 23:56:52 +0100

    MsLuvaLuva acts crazy when people say some certain keywords or phrases.

--- scripts/MESSAGE/xbye.js
+++ scripts/MESSAGE/xbye.js
@@ -5,54 +5,33 @@ if (timeout) delta = new Date().getTime() - timeout;
 if (delta >= 20000) {
 	message = message.toLowerCase();
 	var u, r;
-	if (message.indexOf("melvin") >= 0 && message.indexOf("!who") < 0 && message.length > 10) {
-		/*
-		r = [
-			"Who is pregnant with Melvin's son..."
-		];
-		"All hail Melvin!",
-		"All hail Melvin!",
-		"All hail Melvin! For he's the only ruler!",
-		"Melvin is the greatest!",
-		"Melvin even has a cola named after him, he must be good!",
-		"Melvin is my hero!",
-		"Melvin is my hero! He should be your hero too.",
-		"Melviiiiiiiin!",
-		"Melvin is the only one I will give my virginity to.",
-		"Drink Melvin cola!",
-		response = r[Math.floor(Math.random() * r.length)];
-		response_to = channel;
-		API.setValue("bye.timeout", new Date().getTime()+240000);
-		*/
-	}
 	if (message.indexOf("is having a baby") >= 0
 			|| message.indexOf("is getting a baby") >= 0
 			|| message.indexOf("i'm having a baby") >= 0
 			|| message.indexOf("is preggers") >= 0
 			|| message.indexOf("i'm pregnant") >= 0
-			|| message.indexOf("i'll be a daddy") >= 0
-			|| message.indexOf("i'm gonna be a daddy") >= 0
+			|| message.indexOf("i'll be a dad") >= 0
+			|| message.indexOf("im gonna be a dad") >= 0
+			|| message.indexOf("i'm gonna be a dad") >= 0
+			|| message.indexOf("i'm gonna be a father") >= 0
 			) {
-		response = "Congrats " + nick + "! Babies are fun! And tasty...";
+		response = "Congrats " + nick + "! Babies are fun! I loooove babies.";
 		response_to = channel;
 		API.setValue("bye.timeout", new Date().getTime());
-	}
-	if (message.indexOf("popogeddon") >= 0
+	} else if (message.indexOf("popogeddon") >= 0
 			|| message.indexOf("armageddon") >= 0
-			|| message.indexOf("999") >= 0
 			) {
 		r = [
 			"We shall all die!",
 			"Popogeddon is here!",
-			"I just wish it all was a bad dream...",
+			"I just wish it all was just a bad dream...",
 			"Be afraid... be very afraid.",
 			"We're doomed!",
 		];
 		response = r[Math.floor(Math.random() * r.length)];
 		response_to = channel;
-		API.setValue("bye.timeout", new Date().getTime()+10000);
-	}
-	if (message.indexOf("i gotta go") >= 0
+		API.setValue("bye.timeout", new Date().getTime() + 10000);
+	} else if (message.indexOf("i gotta go") >= 0
 			|| message.indexOf("i got to go") >= 0
 			|| message.indexOf("bye bye") >= 0
 			|| message.indexOf("im leaving now") >= 0
@@ -67,10 +46,12 @@ if (delta >= 20000) {
 			|| message.indexOf("laters every") >= 0
 			) {
 		u = [
+			"All right, bye then $nick!",
 			"Bye byes $nick!",
 			"Good bye, $nick.",
 			"Hope to have you come back sometime soon, $nick.",
 			"I'll be here waiting for your return, $nick.",
+			"I'll be waiting for your to come back soon $nick.",
 			"Happy to have you here, $nick.",
 		];
 		response = u[Math.floor(Math.random() * u.length)].replace(/\$nick/, nick);
@@ -85,16 +66,36 @@ if (delta >= 20000) {
 			"See ya, $nick!",
 			"I'll be waiting for you, $nick.",
 			"Don't be gone for too long, $nick!",
+			"Already miss you lots, $nick.",
 		];
 		response = u[Math.floor(Math.random() * u.length)].replace(/\$nick/, nick);
 		response_to = channel;
 		API.setValue("bye.timeout", new Date().getTime());
+	} else if (message.indexOf("tasty") >= 0) {
+		response = "Your mom was tasty";
+		response_to = channel;
+		API.setValue("bye.timeout", new Date().getTime());
+	} else if (message.indexOf("melvin") >= 0 && message.indexOf("!who") < 0 && message.length > 10) {
+		timeout = API.getValue("melvin.timeout");
+		delta = 360000;
+		if (timeout) delta = new Date().getTime() - timeout;
 
+		if (delta >= 360000) {
+			r = [
+				"All hail Melvin!",
+				"All hail Melvin! For he's the only ruler!",
+				"Melvin is the greatest!",
+				"Melvin even has a cola named after him, so he must be good!",
+				"Melvin is my hero!",
+				"Melvin is my hero! He should be your hero too.",
+				"Melviiiiiiiin!",
+				"Melvin is the only one I will give my virginity to.",
+				"Drink Melvin cola! Or die.",
+			];
+			response = r[Math.floor(Math.random() * r.length)];
+			response_to = channel;
+			API.setValue("melvin.timeout", new Date().getTime());
+		}
 	}
-
 } else {
-	//API.debug("xbye Delta: " + delta);
-	if (message.toLowerCase().indexOf("alanas") >= 0) {
-		API.info("\n\n:    ALANAS MENTIONED\n");
-	}
 }
