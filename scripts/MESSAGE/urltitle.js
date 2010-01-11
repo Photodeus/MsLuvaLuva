61299d256ee6d2ec792be95023b5d37628c6d687
Photodeus <photodeus+github@gmail.com>
2010-01-11 23:54:45 +0100

    Removed dependency to external resource urltitle.jsp. Youtube links are now parsed by the bot and the script itself.

--- scripts/MESSAGE/urltitle.js
+++ scripts/MESSAGE/urltitle.js
@@ -1,7 +1,7 @@
 function endsWith(string, suffix)
 {
-    var lastIndex = string.lastIndexOf(suffix);
-    return (lastIndex != -1) && (lastIndex + suffix.length == string.length);
+	var lastIndex = string.lastIndexOf(suffix);
+	return (lastIndex != -1) && (lastIndex + suffix.length == string.length);
 }
 
 if (message.indexOf('http://') >= 0) {
@@ -22,14 +22,84 @@ if (message.indexOf('http://') >= 0) {
 			// do nothing
 			// API.info("Link ignored: is on disallowed list. ");
 		} else {
-			if (link.indexOf('youtube.com') > 0) {
-				// TODO port youtube title code from urltitle.jsp
-				var param = ""+API.encode(link);
-				var url = "http://popodeus.com/chat/bot/urltitle.jsp?"+param;
-				var title = ""+API.getPageAsText(url)
-				if (title.length > 4) {
-					API.info("Title: " + title);
-					response = title;
+			/*
+			 // Original title code from urltitle.jsp converted into JavaScript
+			 var param = ""+API.encode(link);
+			 var url = "http://popodeus.com/chat/bot/urltitle.jsp?"+param;
+			 var title = ""+API.getPageAsText(url)
+			 if (title.length > 4) {
+			 API.info("Title: " + title);
+			 response = title;
+			 response_to = channel;
+			 cancel = true;
+			 }
+			 */
+			if (link.indexOf('youtube.com/watch') > 0) {
+				var code = link.match(/\?v=([0-9a-zA-Z_\\-]+)/)[1];
+				var output = "";
+				if (code) {
+					var page = API.getPage("http://gdata.youtube.com/feeds/api/videos/" + code);
+					var title = API.getAsText(page, "//media:title");
+					var err = "";
+					var attributes;
+
+					API.info("page: " + page + " title: " + title);
+					try {
+						if (title) {
+							output += title;
+						} else {
+							output += "Title couldn't be fetched. Blame Connie";
+						}
+					} catch (e) { err += e + "\n"; }
+
+					try {
+						var duration = API.getByXpath(page, "//yt:duration");
+						if (duration != null) {
+							attributes = duration.getAttributes();
+							var seconds = parseInt(attributes.getNamedItem("seconds").getNodeValue());
+							var minutes = Math.floor(seconds / 60);
+							if (minutes >= 1) { seconds = seconds % 60; }
+							output += " [" + (minutes < 10 ? "0" : "") + minutes + ":" + (seconds < 10 ? "0" : "") + seconds + "]";
+						}
+					} catch (e) { err += e + "\n"; }
+
+					try {
+						var rating = API.getByXpath(page, "//gd:rating");
+						if (rating != null) {
+							attributes = rating.getAttributes();
+							var r = Math.round(parseFloat(attributes.getNamedItem("average").getNodeValue()) * 100.) / 100.;
+							var votes = API.formatNum(parseInt(attributes.getNamedItem("numRaters").getNodeValue()));
+							output += " Avg.rating: " + r + "★ of " + votes + " votes.";
+						}
+					} catch (e) { err += e + "\n"; }
+
+					try {
+						var stats = API.getByXpath(page, "//yt:statistics");
+						if (stats != null) {
+							attributes = stats.getAttributes();
+							var vc = parseInt(attributes.getNamedItem("viewCount").getNodeValue());
+							var fc = parseInt(attributes.getNamedItem("favoriteCount").getNodeValue());
+							output += " Viewed: " + API.formatNum(vc) + " times, favorited: " + API.formatNum(fc) + " times.";
+						}
+					} catch (e) { err += e + "\n"; }
+
+					try {
+						var state = API.getByXpath(page, "//yt:state[@name=\"restricted\"]");
+						if (state != null) {
+							output += ". " + state.asText();
+						}
+					} catch (e) { err += e + "\n"; }
+
+					try {
+						var name = API.getByXpath(page, "//author/name");
+						if (name != null) {
+							output += ". By " + name.asText();
+						}
+					} catch (e) { err += e + "\n"; }
+
+					if (err.length > 0) API.info(err);
+
+					response = output;
 					response_to = channel;
 					cancel = true;
 				}
@@ -37,7 +107,7 @@ if (message.indexOf('http://') >= 0) {
 				var page = API.getPage(link);
 				var title = page.getTitleText();
 				if (title != "") {
-					response = ""+title;
+					response = "" + title;
 					response_to = channel;
 					cancel = true;
 				}
@@ -47,7 +117,7 @@ if (message.indexOf('http://') >= 0) {
 } else if (message.match(/\d{5,8}\.\d{1,4}/)) {
 	var id = message.match(/(\d{5,8}\.\d{1,4})/)[1];
 	if (parseInt(id.split('.')[1]) <= 1000) {
-		var line = ""+API.getPageAsText('http://popodeus.com/forum-search/findthreadbyid.jsp?id='+id);
+		var line = "" + API.getPageAsText('http://popodeus.com/forum-search/findthreadbyid.jsp?id=' + id);
 		if (line.length > 4) {
 			response = line;
 			response_to = channel;
