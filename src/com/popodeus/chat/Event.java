4079188cb03c492356fb9ee728876766c154c66c
Photodeus <photodeus+github@gmail.com>
2010-01-13 07:30:08 +0100

    Event interface moved to separate file from class MsLuvaLuva.java

---
+++ src/com/popodeus/chat/Event.java
@@ -0,0 +1,19 @@
+package com.popodeus.chat;
+
+/**
+ * photodeus
+ * Jan 13, 2010
+ * 7:29:11 AM
+ */
+enum Event {
+	JOIN,
+	MESSAGE,
+	ACTION,
+	NICKCHANGE,
+	RIGHTS,
+	KICK,
+	LEAVE,
+	TOPIC,
+	QUIT,
+	UNSUPPORTED,;
+}
