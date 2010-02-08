459e16a246d9a7cc3bafdfccb6415e7c13c82d94
Photodeus <photodeus+github@gmail.com>
2010-02-08 23:48:42 +0100

    option to opt out from stalking... doesn't work, someone can fix it if they like

--- scripts/stalk.js
+++ scripts/stalk.js
@@ -1,79 +1,88 @@
 // @description it's really just a !seen command, that asks the bot when someone was seen last online saying something
 // @example !seen nickname
-function relativeTime(time) {
-	var delta = Math.floor(new Date().getTime() / 1000 - time);
+function relativeTime(time, sayAgo) {
+	var delta = parseInt(new Date().getTime() / 1000 - time);
 	var d, h, m, s;
+	var ago = sayAgo ? " ago" : "";
 	if (delta <= 60) {
-		datemsg = Math.floor(delta) + " seconds ago";
+		datemsg = Math.floor(delta) + " seconds" + ago;
 	} else if (delta < 60 * 60) {
 		m = Math.floor(delta / 60);
 		s = Math.floor(delta) % 60;
-		datemsg = m + " min " + s + " sec ago";
+		datemsg = m + " min " + s + " sec" + ago;
 	} else if (delta < 24 * 60 * 60) {
 		h = parseInt(delta / 3600);
 		m = parseInt(delta / 60) % 60;
 		s = delta % 60;
 		//datemsg = h + " h and " + m + " min and " + s + "s ago";
-		datemsg = h + " h and " + m + " min ago";
+		datemsg = h + " h and " + m + " min" + ago;
 	} else {
 		d = Math.floor(delta / 86400);
 		h = Math.floor(delta / 3600) - d * 24;
 		m = Math.floor(delta / 60) - d * 1440 - h * 60;
 		s = Math.floor(delta) % 60;
-		datemsg = d + " days and " + h + " hours ago";
+		datemsg = d + " days and " + h + " hours" + ago;
 	}
 	return datemsg;
 }
 
-var lnick = param.toLowerCase();
+var lnick = param.toLowerCase().replace(/_$/, '');
 var nostalk = API.getValue("nostalk." + lnick);
 
 if (param == nick) {
-	// ignore this
-	API.notice(nick, 'Yes? Seen who, other than yourself. Others can stalk you: ' + nostalk);
+
+	API.notice(nick, 'Others can stalk you: ' + (nostalk ? "No" : "Yes (default)"));
+	no_timeout = true;
+
+} else if (param == '-help') {
+
+	response = "To turn off !stalking of your nickname, please type in !stalk -off. To enable it again, use !stalk -on";
+	response_to = channel;
 	no_timeout = true;
 
 } else if (param == '-off') {
-	API.setValue("nostalk." + lnick, 1);
-	//API.info(nick + " opted out from stalking");
-	no_timeout = true;
+	API.setValue("nostalk." + lnick, "on");
+	API.info(nick + " opted out from stalking");
 	response = "OK: Will not let others stalk you " + nick;
 	response_to = channel;
-
-} else if (param == '-on') {
-	API.setValue("nostalk." + lnick, 0);
-	//API.info(nick + " opted in to stalking");
 	no_timeout = true;
 
+} else if (param == '-on') {
+
+	API.removeValue("nostalk." + lnick);
+	API.info(nick + " is opting in to stalking");
 	response = "OK: Stalking enabled again, " + nick;
 	response_to = channel;
+	no_timeout = true;
 
 } else {
 
-	//API.info("nostalk."+lnick+": " + nostalk);
-	if (nostalk || lnick.indexOf('alanas') >= 0 || lnick.indexOf('photodeus') >= 0 ) {
-		response = "I'll be back in January 2010.";
-		API.info("\n\n" + nick + " tried to stalk " + param + "\n\n");
+	if (nostalk) {
+		response = "This user is not tracked. For more information, /msg " + API.getBotNick() + " !stalk -help";
 	} else {
 
-		var time = API.getValue("seen." + lnick + ".time");
+		var time = API.getValue("seen." + lnick + ".time"); // time is seconds, not millis
 		var msg = API.getValue("seen." + lnick + ".msg");
-		var qtime = API.getValue('seen.' + lnick + '.quit.time');
+		var qtime = API.getValue('seen.' + lnick + '.quit.time'); // seconds
 		var qmsg = API.getValue('seen.' + lnick + '.quit.msg');
 		//API.info(time + ", q: " + qtime);
 
 		if (time && msg) {
-			var dmsg = relativeTime(time);
+			var dmsg = relativeTime(time, true);
 			var qdata = "";
 			if (qtime && qtime > time) {
-				qdata = " (" + relativeTime(qtime) + ": " + qmsg + ")";
+				qdata = " (" + relativeTime(qtime, true) + ": " + qmsg + ")";
 			}
 			response = param + " was seen " + dmsg + " saying \"" + msg + "\"" + qdata;
 		} else {
-			response = "Sorry, no memory of " + param;
+			if (param.match(/your /)) param = param.replace("your ", "my ");
+			response = "Sorry, I haven't seen " + param + " after me joining channel " + relativeTime(API.getStartupTime() / 1000, true) + ".";
+			time = API.getLastActiveTime(param, channel);
+			if (time) {
+				time = time.longValue() / 1000;
+				response += " Last log entry I have is " + relativeTime(time, false) + " old.";
+			}
 		}
 	}
 	response_to = channel;
 }
-
-//API.clearScriptCacheFor("stalk");
