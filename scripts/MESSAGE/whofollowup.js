79491021e6c089db4f8f9f93cd8c0c1d56809d13
Photodeus <photodeus+github@gmail.com>
2010-02-09 03:44:19 +0100

    support for whod.js also

--- scripts/MESSAGE/whofollowup.js
+++ scripts/MESSAGE/whofollowup.js
@@ -1,21 +1,29 @@
 // Listens to lines consisting of only a number
 if (message.match(/^\d+$/)) {
-	// starts with a number
 	var lnick = "" + nick.toLowerCase();
-	var lastq = API.getValue("who."+lnick+"!"+ident+"@host");
-	var time = API.getValue("who.time."+lnick+"!"+ident+"@host");
-	//API.info(nick + " lastq => " + lastq);
+	// Check values previously set in who.js and whod.js
+	var lastq = API.getValue("who."+lnick+"!"+ident+"@"+host);
+	var time = API.getValue("who.time."+lnick+"!"+ident+"@"+host);
+	// API.info(nick + " lastq => " + lastq);
+	// We expect a reply within 30 seconds, or otherwise we just ignore the command
 	if (lastq && time && (new Date().getTime()/1000-time < 30)) {
-		var p = API.encode(message + " " + lastq);
-		var line = API.getPageAsText("http://popodeus.com/namesearch/find.jsp?q="+p);
+		var url = "http://popodeus.com/namesearch/find.jsp?";
+		// New query is same as calling !who number lastmessage
+		var q = "q="+ API.encode(message + " " + lastq);
+		if (API.getValue("who."+lnick+".dflag")) {
+			// Did we look for deceaced chars
+			q = q + "&deceased=include";
+		}
+		// Query the external name database 
+		var line = API.getPageAsText(url + q);
 		//API.info(line);
 		if (line != null) {
 			API.say(channel, line);
+			// No further processing of other scripts
 			cancel = true;
-			API.setValue("who."+lnick+"!"+ident+"@host", null);
+			// Cleanup
+			API.removeValue("who."+lnick+"!"+ident+"@"+host);
+			API.removeValue("who."+lnick+".dflag");
 		}
 	}
 }
-// No more who queries after this
-//API.removeValue("who."+lnick+"!"+ident+"@host");
-//API.setValue("who."+lnick+"!"+ident+"@host", null);
