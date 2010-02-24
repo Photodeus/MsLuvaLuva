5fc2b79a0ee7505cda2a1f1339d5f1e49fcfd52c
Photodeus <photodeus+github@gmail.com>
2010-02-24 12:21:03 +0100

    use parseInt

--- scripts/ACTION/action.js
+++ scripts/ACTION/action.js
@@ -1,6 +1,6 @@
 var lnick = ""+nick.toLowerCase();
 
-API.setValue("seen."+lnick + ".time", Math.floor(new Date().getTime()/1000));
+API.setValue("seen."+lnick + ".time", parseInt(new Date().getTime()/1000));
 API.setValue("seen."+lnick + ".msg", message);
 
 if (message.indexOf(' snuggles ') > 0) {
