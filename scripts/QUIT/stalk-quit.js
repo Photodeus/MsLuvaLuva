699c21f6392a9520934a81f5a9b64e2321dcff1c
Photodeus <photodeus+github@gmail.com>
2010-02-24 12:19:14 +0100

    use parseInt

--- scripts/QUIT/stalk-quit.js
+++ scripts/QUIT/stalk-quit.js
@@ -1,4 +1,4 @@
 var lnick = ""+nick.toLowerCase();
 
-API.setValue("seen."+lnick + ".quit.time", Math.floor(new Date().getTime()/1000));
+API.setValue("seen."+lnick + ".quit.time", parseInt(new Date().getTime()/1000));
 API.setValue("seen."+lnick + ".quit.msg", message);
