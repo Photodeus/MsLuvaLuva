8000d2a89a38a4536f10593f3b7ecde483da69c7
Photodeus <photodeus+github@gmail.com>
2010-06-10 01:14:36 +0200

    remove any commas from the expressions

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,6 +1,7 @@
-// @description Calculates given expression
+// @description Calculates given expression. You can only use <code>+ - * / ( ) 0-9</code> and hexadecimal <code>0xA-F</code>
 // @example  !calc 4.5*(12-5/8)
 //var filtered = ""+param.match( /[0-9.\/\+\-\*\(\) \^\&\|\<\>xa-fA-F]+/ );
+param = param.replace( /,/g, '' );
 var filtered = ""+param.match( /[0-9.a-fA-F\/\+\-\*\(\) \^e%x]+/ );
 if (filtered.indexOf('^') > 0) {
 	filtered = filtered.replace( /(.+?)\^(.+?)/g, 'Math.pow($1, $2)' );
