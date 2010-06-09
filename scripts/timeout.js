c2b9a03a685190b14c53717e049a349629e4c0cf
Photodeus <photodeus+github@gmail.com>
2010-06-10 01:37:46 +0200

    show timeout instead of whining about privileges

--- scripts/timeout.js
+++ scripts/timeout.js
@@ -1,5 +1,6 @@
 // @description Op command only. Allows to set the minimum timeout in seconds between bot commands
-// @example !timeout 10 *
+// @example !timeout
+// @example !timeout 10
 // @example !timeout 10 command
 // @notimeout
 if (API.isOp(channel, nick) || API.isOwner(channel, nick) || API.isAdmin(channel, nick)) {
@@ -8,11 +9,13 @@ if (API.isOp(channel, nick) || API.isOwner(channel, nick) || API.isAdmin(channel
 		if (time > 0 && time <= 120) {
 			API.notice(nick, "Ok: Setting command timeout to " + time + " seconds");
 			API.setTimeout("*", time * 1000);
-			no_timeout = true;
+		} else {
+			API.say(channel, "Command timeout must be between 1 and 120 seconds");
 		}
 	} else {
-		API.say(channel, "Command timeout is " + API.getTimeout("*")/1000 + " seconds");
+		API.say(channel, "Command timeout is " + (API.getTimeout("*")/1000) + " seconds");
 	}
 } else {
-	API.notice(nick, 'Not enough privileges to do that.');
+	API.say(channel, "Command timeout is " + (API.getTimeout("*")/1000) + " seconds");
 }
+no_timeout = true;
