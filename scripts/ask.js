d7424cea02b548c2d8ded1faeb38d4a5ac428b73
Photodeus <photodeus+github@gmail.com>
2010-06-10 01:13:36 +0200

    longer reply

--- scripts/ask.js
+++ scripts/ask.js
@@ -1,3 +1,3 @@
 // @description Don't ask to ask... just ask it.
-response = "Don't ask to ask... just ask it.";
+response = "Please don't ask to ask... just ask it. If your question is interesting and thought-provoking, you may earn a reply.";
 response_to = channel;
