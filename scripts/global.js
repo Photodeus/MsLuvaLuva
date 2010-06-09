8b02826ba79dada5d047123a0330517594842320
Photodeus <photodeus+github@gmail.com>
2010-06-10 01:22:06 +0200

    more nasty attitude to response

--- scripts/global.js
+++ scripts/global.js
@@ -1,4 +1,4 @@
-// @description How to find that silly #Global chat
+// @description How to find that silly #Global chat nobody ever uses
 response_to = channel;
-response = "If you really wanna know, it's irc://chat.popmundo.com:7000/#Global";
+response = "If you really wanna know, it's irc://chat.popmundo.com:7000/#Global Nobody will speak with you if you go there.";
 
