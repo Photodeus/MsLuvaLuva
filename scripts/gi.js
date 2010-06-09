42b93e3d78bc1239b0c7f52675682e81ae0d3277
Photodeus <photodeus+github@gmail.com>
2010-06-10 01:21:39 +0200

    safe mode for image search

--- scripts/gi.js
+++ scripts/gi.js
@@ -1,4 +1,4 @@
 // @description Gives a link to Google Image Search
 // @example !gi costume party
 response_to = channel
-response = 'http://images.google.com/images?q='+API.encode(param);
+response = 'http://images.google.com/images?safe=strict&q='+API.encode(param);
