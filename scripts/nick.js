817c226e97195da943306c9b10a549772c7213a8
Photodeus <photodeus+github@gmail.com>
2010-06-10 01:28:13 +0200

    script to explain how to change the nick inside the chat

---
+++ scripts/nick.js
@@ -0,0 +1,5 @@
+// @description explains how to change your nick inside the chat
+// @example !nick
+var CLR = String.fromCharCode(3);
+response_to = channel;
+response = "To change your nickname, just write "+CLR+"4/nick NameLastname"+CLR+" The / is part of the command, you have to type it in also.";
