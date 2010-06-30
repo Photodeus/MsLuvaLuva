e2ee895fda79c2763ffdb6aba44a1ff72e3fb757
Seppo Vuolteenaho <photodeus@gmail.com>
2010-06-30 19:23:10 +0200

    Anchor needs the rule- prefix

--- scripts/rule.js
+++ scripts/rule.js
@@ -2,8 +2,8 @@
 // @example !rule 3.4
 var m = param.match(/(\d\.\d)/);
 if (m) {
-	response = 'http://popodeus.com/chat/rules/#' + m[1];
+	response = 'http://popodeus.com/chat/rules/#rule-' + m[1];
 	response_to = channel;
 } else {
-	API.notice(nick, m);
+	API.notice(nick, "Unrecognized rule. It should be in the form of n.n where n is a number");
 }
