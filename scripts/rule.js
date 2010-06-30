d31b1a246627c2086fdd36ecf728d09c1cac5c27
Photodeus <photodeus+github@gmail.com>
2010-06-30 19:20:40 +0200

    Match on /\d\.\d/ instead of /rule \d\.\d/

--- scripts/rule.js
+++ scripts/rule.js
@@ -1,7 +1,9 @@
 // @description Link to rule x.x
 // @example !rule 3.4
-var m = param.match(/rule.(\d\.\d)/);
+var m = param.match(/(\d\.\d)/);
 if (m) {
 	response = 'http://popodeus.com/chat/rules/#' + m[1];
 	response_to = channel;
+} else {
+	API.notice(nick, m);
 }
