9bc642ed4dd05203c9776f6075e1e589841f73de
Photodeus <photodeus+github@gmail.com>
2010-07-27 08:08:52 +0200

    Works now, but needs web data caching

--- scripts/confess.js
+++ scripts/confess.js
@@ -1,14 +1,22 @@
-// @description Spill the beans on someone
-API.clearScriptCacheFor('confess');
+// @description Tell everyone a sad sad confession.
+// @level adult
 
-var c = API.getValue("confessions");
-if (!c) {
-	var page = API.getPage("http://confessions.grouphug.us/random");
-	if (page) {
-		var div = API.getElementById(page, "squeeze");
-		if (div) {
-			var list = page.getElementsByAttribute(div, "p", "", "");
-			response = ""+list.get(0);
+var p = API.getValue("confessions");
+if (!p) {
+	// TODO use up all 10 random confessions before pulling new page
+	p = API.getPage("http://confessions.grouphug.us/random");
+	// API.setValue("confessions", p);
+}
+
+if (p) {
+	var div = API.getFirstByXpath(p, "//div[@class='content']/p");
+	if (div) {
+		var msg = (""+div.asText()).replace(/\u8216|&#8216;/g, "'");
+		if (msg.match(/cancer|died|kill myself/i)) {
+			response = "Mmm, sorry. No new confessions have arrived since last time."; 
+			response_to = channel;
+		} else {
+			response = msg;
 			response_to = channel;
 		}
 	}
