7974e2e9ce002bbffd032051467d4187049f5295
Photodeus <photodeus+github@gmail.com>
2010-07-27 08:18:07 +0200

    Modified some responses and added a couple new ones

--- scripts/ass.js
+++ scripts/ass.js
@@ -4,8 +4,11 @@ var r = [
 	"I bet you inherited yours from a baboon, $nick!",
 	"Spankable, baby! Spankable!",
 	"Synonym for you, $nick!",
-	"That'll make 50 dollars $nick. Cash up front.",
+	"Stop shouting your nickname in here, it makes you look silly.",
+	"Yea? If you want some, that'll make 150 dollars $nick. Cash up front.",
 	"Who are you trying to impress, $nick. You're just being an ass today.",
+	"Well aren't we a bit childish today?",
+	"I bet you'd fancy some...",
 ];
 response = r[Math.floor(Math.random()*r.length)].replace(/\$nick/, nick);
 response_to = channel;
