126e516ba5b57dc834453df2783561de1c6f90ad
Photodeus <photodeus+github@gmail.com>
2010-07-27 08:05:32 +0200

    Randomly be positive or negative in response

--- scripts/awesome.js
+++ scripts/awesome.js
@@ -1,2 +1,6 @@
-response = "Oh yes you are!";
+var r = [
+	"Oh yes you are $nick!",
+	"You wish you were, $nick...",
+];
+response = r[Math.floor(Math.random()*r.length)].replace(/\$nick/, nick);
 response_to = channel;
