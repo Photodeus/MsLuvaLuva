ee3a202ff8b0ae1f8a1dd64340f7ece2535d9285
Photodeus <photodeus+github@gmail.com>
2010-07-27 08:13:48 +0200

    More details into script description

--- scripts/greet.js
+++ scripts/greet.js
@@ -1,4 +1,5 @@
-// @description Greet someone
+// @description Greet someone. The bot automatically greets everyone who joins chat using this same script.
+// @example !greet Jonas
 //API.notice(nick, API.greetings.size());
 response = ""+API.getGreeting().replaceAll( "\\$nick", param );
 response_to = channel;
