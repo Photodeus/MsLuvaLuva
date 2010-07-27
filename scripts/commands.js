181b92225cf9d6a5a5797de208b6185c2e53c458
Seppo Vuolteenaho <photodeus@gmail.com>
2010-07-27 09:15:46 +0200

    Weather is nowadays called just w

--- scripts/commands.js
+++ scripts/commands.js
@@ -1,3 +1,3 @@
 // @description Short list of most useful commands
 response_to = channel
-response = "who, asl, assets, stalk, weather, fc (forecast), calc, fs (forum search), g (google), gi (google images), random. For more, use !help"
+response = "who, whod, asl, assets, stalk, w (weather), fc (forecast), calc, fs (forum search), g (google), gi (google images), random. For more, use !help"
